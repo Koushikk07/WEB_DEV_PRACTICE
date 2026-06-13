54aa0df60d094bcaf21a67b5637aadaacb9fd3a9
Gudapur Koushik <tgznotaxid@gmail.com>
2026-06-13 21:15:20 +0530

    handling errors , await async , api basic

---
+++ JS/api.js
@@ -0,0 +1,23 @@
+/* 
+API - Application programming interface
+
+demo;
+amazon.in (search bar) => req => amazon api => response => html/css/js => shows up the browser
+
+api asks a waiter in hotel (as per srad.di)
+
+as for now we are just see how to use apis (communicate b/w to two softwares)
+
+apis return the json data
+
+api basically url also know as link(endpoint)
+
+
+
+some random API's
+catfact.ninja/fact   -- sends random cat facts
+boreapi  -- sends an aactivity to do when bored
+dog.ceo -- sends dog pics
+
+
+*/
--- JS/async.js
+++ JS/async.js
@@ -44,5 +44,70 @@ greet()
     */
 
 function getNum() {
-  console;
+  setTimeout(() => {
+    return new Promise((resolve, reject) => {
+      let num = Math.floor(Math.random() * 10) + 1;
+      return num;
+    }, 1000);
+  }); //console.log(5);
 }
+
+async function demo() {
+  await getNum(); // until this funtion not return , other function will not execute
+  getNum();
+  getNum();
+}
+
+/* 
+we know funtion demo ()
+{
+change color("red",1000); //1 sec delay
+change color("green",1000); //1 sec delay
+change color("orange",1000); //1 sec delay
+change color("red",1000); //1 sec delay
+change color("blue",1000); //1 sec delay
+but it directly shows blue in h1. 
+
+}
+
+*/
+
+h1 = document.querySelector("h1");
+
+function changeColor(color, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // for reject -- let num = math.floor .. if num >3 reject("promise rejected")
+      h1.style.color = color;
+      console.log(`color changed to ${color}!`);
+      resolve("color changed!");
+    }, delay);
+  });
+}
+
+async function demo() {
+  await changeColor("red", 1000);
+  await changeColor("green", 1000);
+  await changeColor("yellow", 1000);
+  await changeColor("blue", 1000);
+  await changeColor("pink", 1000);
+  await changeColor("grey", 1000);
+}
+
+// now it prints each color for 1sec then next color will.
+
+//this case has no error
+// what if any errors
+// if any of function is rejected like yellow func rest of the functionw will not executed
+
+/* 
+Handing Rejections
+
+try{
+....await   this will make sure if one will fail rest will execute
+
+}catch(err)
+{
+console.log(err);
+}
+*/
