592fe2794b75ab68e5f099fcc92c6ecd3a70295c
Gudapur Koushik <tgznotaxid@gmail.com>
2026-07-06 21:20:38 +0530

    recap , oops, get post

---
+++ oops/index.js
@@ -0,0 +1,17 @@
+/* 
+
+GET
+
+-> used to GET some response
+
+-> Data sent in query strings
+(limited, string data & visible in URL)
+
+
+POST
+
+->used to POST something (for create/write/update)
+
+->data sent via request body (any type of data)
+
+*/
