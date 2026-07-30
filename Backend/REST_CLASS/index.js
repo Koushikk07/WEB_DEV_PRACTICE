e47ebd33909d1ba2925481a3cd4ee0198c168bce
Gudapur Koushik <tgznotaxid@gmail.com>
2026-07-30 20:50:33 +0530

    REST APIs - CRUD operations completed

--- Backend/REST_CLASS/index.js
+++ Backend/REST_CLASS/index.js
@@ -2,57 +2,60 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const path = require("path");
+var methodOverride = require("method-override");
+const { v4: uuidv4 } = require("uuid");
+uuidv4();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(methodOverride("_method"));
 let posts = [
   {
-    id: "1a", //its auto. creates in db
+    id: uuidv4(), //its auto. creates in db
     username: "Ram_Charan",
     content: "I love coding",
   },
   {
-    id: "2a",
+    id: uuidv4(),
     username: "Prabhas_Raju",
     content: "I love food, thanks to thanks a lot",
   },
   {
-    id: "3a",
+    id: uuidv4(),
     username: "Allu_arjun",
     content: "naku Dulla ekuva, bhai bolthe",
   },
   {
-    id: "4a",
+    id: uuidv4(),
     username: "Vijay_Devarakonda",
     content: "Fashion Designer | actor",
   },
   {
-    id: "5a",
+    id: uuidv4(),
     username: "Mahesh_Babu",
     content: "Babu lake babu",
   },
   {
-    id: "6a",
+    id: uuidv4(),
     username: "Akhil",
     content: "ayyagare no.1",
   },
   {
-    id: "7a",
+    id: uuidv4(),
     username: "Surya",
     content: "ROLEX | Most wanted Criminal",
   },
   {
-    id: "8a",
+    id: uuidv4(),
 
     username: "Nani",
     content: "currently working on paradise.",
   },
   {
-    id: "8a",
+    id: uuidv4(),
     username: "Karthik",
     content: "Evaru ra miru antha ",
   },
@@ -76,9 +79,30 @@ app.get("/posts/:id", (req, res) => {
   /* console.log(id);
   res.send("request is working"); */
   let post = posts.find((p) => id === p.id);
-  console.log(post);
+  res.render("show.ejs", { post });
 });
 
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((p) => id === p.id);
+  post.content = newContent;
+  console.log(newContent);
+  // res.send("PATCH request working");
+  res.redirect("/posts");
+});
+
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => id !== p.id);
+  //res.send("delete successfully");
+  res.redirect("/posts");
+});
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("edit.ejs", { post });
+});
 app.listen(port, () => {
   console.log("listening to port:8000");
 });
