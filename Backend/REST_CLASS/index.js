5e12940ac16bc764937ede6857ec18cd05e44b9f
Gudapur Koushik <tgznotaxid@gmail.com>
2026-07-30 17:15:20 +0530

    REST APIs - operations

--- Backend/REST_CLASS/index.js
+++ Backend/REST_CLASS/index.js
@@ -6,41 +6,53 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 let posts = [
   {
-    username: "Ram Charan",
+    id: "1a", //its auto. creates in db
+    username: "Ram_Charan",
     content: "I love coding",
   },
   {
-    username: "Prabhas Raju",
+    id: "2a",
+    username: "Prabhas_Raju",
     content: "I love food, thanks to thanks a lot",
   },
   {
-    username: "Allu arjun",
+    id: "3a",
+    username: "Allu_arjun",
     content: "naku Dulla ekuva, bhai bolthe",
   },
   {
-    username: "Vijay Devarakonda",
+    id: "4a",
+    username: "Vijay_Devarakonda",
     content: "Fashion Designer | actor",
   },
   {
-    username: "Mahesh Babu",
+    id: "5a",
+    username: "Mahesh_Babu",
     content: "Babu lake babu",
   },
   {
+    id: "6a",
     username: "Akhil",
     content: "ayyagare no.1",
   },
   {
+    id: "7a",
     username: "Surya",
     content: "ROLEX | Most wanted Criminal",
   },
   {
+    id: "8a",
+
     username: "Nani",
     content: "currently working on paradise.",
   },
   {
+    id: "8a",
     username: "Karthik",
     content: "Evaru ra miru antha ",
   },
@@ -48,6 +60,23 @@ let posts = [
 
 app.get("/posts", (req, res) => {
   res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  posts.push({ username, content });
+  res.redirect("/posts");
+});
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  /* console.log(id);
+  res.send("request is working"); */
+  let post = posts.find((p) => id === p.id);
+  console.log(post);
 });
 
 app.listen(port, () => {
